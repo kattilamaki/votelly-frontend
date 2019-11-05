892004921b1eb997df80137cac2523bcd3924e06
kattilamaki <markus@kattilamaki.com>
2019-11-05 15:08:16 +0200

    switch to real api calls (side effects)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import './App.css';
 import Program from './Program/Program';
 import Character from './Character/Character';
@@ -7,45 +7,27 @@ import Character from './Character/Character';
 class App extends Component {
 
   state = {
-    programs: [{
-      "name": "Test program 1",
-      "description": "This is some test program that is for the development purposes. It is well and nice and everything else also!",
-      "image_alt_text": "some image",
-      "vote_question": "Is this program nice and fine for the stuff that so and that?"
-  }, {
-      "name": "Test program 2",
-      "description": "Some other description",
-      "image_alt_text": "sdfafdsaas",
-      "vote_question": "HOW IS IT?"
-  }],
-    characters: [{
-      "name": "Character 1",
-      "description": "First character for the votelly testing",
-      "image_alt_text": "Anna askar photo",
-      "number_of_votes": 0,
-      "related_program": 1,
-      "character_photo": "http://localhost:8000/pics/media/anna.png"
-    }, {
-      "name": "Character 2",
-      "description": "Second character for the votelly testing",
-      "image_alt_text": "Anna askar photo",
-      "number_of_votes": 0,
-      "related_program": 1,
-      "character_photo": "http://localhost:8000/pics/media/anna.png"
-    }],
+    programs: [],
+    characters: [],
     characterComments: [],
     selectedProgram: 0,
     showCharacters: false
   }
-/*
+
   componentDidMount() {
-    axios.get('http://localhost:1234/api/programs/')
+    axios.get('http://localhost:8000/api/programs/')
     .then(response => {
       this.setState({programs: response.data.data});
-      console.log(this.state.programs);
     });
   }
-*/
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate!")
+    axios.get('http://localhost:8000/api/program/' + this.state.selectedProgram + '/characters/')
+    .then(response => {
+      this.setState({characters: response.data.data});
+    });
+  }
 
 toggleCharactersHandler = (programId) => {
   const doesShow = this.state.showCharacters;
@@ -68,15 +50,15 @@ toggleCharactersHandler = (programId) => {
     );
   }
 
-    const programs = this.state.programs.map((program, index) => {
+    const programs = this.state.programs.map(program => {
       return <Program
       name={program.name}
       description={program.description}
-      clicked={() => this.toggleCharactersHandler(index)}
-      programId={index + 1}
+      programId={program.id}
+      clicked={() => this.toggleCharactersHandler(program.id)}
       />
     });
-  
+
     return (
       <div>
         <h1>{this.props.title}</h1>
