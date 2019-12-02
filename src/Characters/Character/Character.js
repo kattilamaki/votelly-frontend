83dd2eb88b5cf05695e3b43791e29aaa5b0f9941
kattilamaki <markus@kattilamaki.com>
2019-12-02 16:34:30 +0200

    styling changes like cards, shadow and hover

--- src/Characters/Character/Character.js
+++ src/Characters/Character/Character.js
@@ -4,11 +4,10 @@ import classes from './Character.css';
 const character = (props) => {
 
     return (
-        <div className={classes.Person}>
+        <div className="character">
         <h2 onClick={props.clicked}>{props.name}</h2>
         <img className="characterImage" src={props.imageUrl} alt={props.imageAltText} onClick={props.clicked} />
         <p>{props.description}</p>
-        <p>{props.numberOfVotes}</p>
         <button onClick={props.voted}>Vote!</button>
         </div>
     );
